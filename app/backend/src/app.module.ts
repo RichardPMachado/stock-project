f01050c1e6a296a7bccf4cdd00b34764356db403
RichardPMachado <richardmachado2211@gmail.com>
2023-05-31 18:45:25 -0300

    chore[Auth]: implement module

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -7,9 +7,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 // import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, UserModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, UserModule, AuthModule],
   controllers: [],
   providers: [PrismaService],
 })
