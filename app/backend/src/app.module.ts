6833fc72318422a2f5630eea5a2317a1750b72ce
RichardPMachado <richardmachado2211@gmail.com>
2023-06-01 18:21:16 -0300

    feat[auth]: implement login, guard and localStrategy

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -3,11 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 // import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), PrismaModule, UserModule, AuthModule],
